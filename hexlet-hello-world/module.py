5f64b9e3141db40504176ff7ee65f1ca2f2ca84a
vitaliiIT <viza.uz.work@gmail.com>
2024-12-24 16:08:38 +0200

    Описание изменений

--- hexlet-hello-world/module.py
+++ hexlet-hello-world/module.py
@@ -1,7 +1,2 @@
-from more_itertools import sliced, substrings
-
-subs = ["".join(s) for s in substrings("more")]
-print(subs)
-
-slices = list(sliced((1, 2, 3, 4, 5, 6, 7, 8), 3))
-print(slices)
+def greet():
+    print('Welcome to my application!')
